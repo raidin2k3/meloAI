e0ca55e988b3cdf16ebb488c62b3cc2886b8bc55
Pranshu Mishra <noidafam@gmail.com>
2024-11-24 00:42:09 +0530

    updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TextInput, TouchableWithoutFeedback, Keyboard } from "react-native";
+import React, { useRef } from "react";
 
 export default function Index() {
+  const inputRef = useRef<TextInput>(null);
+
+  const handlePress = () => {
+    inputRef.current?.focus();
+  };
   return (
     <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={styles.holder}>
+        <Text style={styles.text1}>Search</Text>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+          <TextInput style={styles.inputHolder} value="What's on your mind?" ref={inputRef}></TextInput>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 }
@@ -13,6 +24,37 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'black'
+    backgroundColor: 'black',
+    justifyContent: 'flex-start',
+  },
+  holder: {
+    backgroundColor: 'black',
+    height: 160,
+    width: 325,
+    borderWidth: 0.75,
+    borderRadius: 30,
+    borderColor: 'grey',
+    marginTop: 30,
+    elevation: 30,
+    shadowColor: 'lightgreen',
+    alignItems: 'center',
+    alignContent:'center'
+  },
+  text1:{
+    color: 'white',
+    padding: 30,
+    textAlign: 'center',
+    fontFamily: 'sans-serif',
+    fontSize: 14
+  },
+  inputHolder:{
+    height: 55,
+    width: 275,
+    borderWidth: 0.75,
+    borderColor: 'grey',
+    borderRadius: 50,
+    color: 'grey',
+    fontSize: 12,
+    paddingLeft: 25,
   }
 })
